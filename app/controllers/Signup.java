64869a2bccc78acd5faaea385e9dd2c42fcb7876
Eduardo Almeida <me@edr.io>
2015-10-20 13:06:30 +0100

    Added some debug code. ^7

--- app/controllers/Signup.java
+++ app/controllers/Signup.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import models.User;
+import models.*;
 import play.mvc.*;
 import play.data.*;
 
@@ -18,7 +18,33 @@ public class Signup extends Controller {
 
         String email = form.get("email");
 
-        User u = new User(user, pass, email);
+        //  User u = new User(user, pass, email);
+
+        /*public User(String username, String password, String emailAddress) {
+        this.id = Integer.toUnsignedLong(10);
+
+        this.username = username;
+
+        try {
+            this.password = new Password(password);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        this.emailAddress = emailAddress;
+    }*/
+
+        User u = new User();
+
+        u.setUser(user);
+
+        try {
+            u.setPassword(new Password(pass));
+        } catch (Exception e) {
+
+        }
+
+        u.setEmailAddress(email);
 
         //  Insert in DB?
 
