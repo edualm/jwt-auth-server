d42f8a33ab6b4e1da5abb1d81162f2d7b72b6347
Eduardo Almeida <me@edr.io>
2015-10-20 17:33:07 +0100

    Changed database schema.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,9 +5,9 @@
 CREATE TABLE user_data (
   id  SERIAL,
   username  VARCHAR(64) NOT NULL,
-  passwordDigest  VARCHAR(256) NOT NULL,
-  passwordSalt VARCHAR(256) NOT NULL,
-  emailAddress VARCHAR(256) NOT NULL,
+  password_digest  VARCHAR(256) NOT NULL,
+  password_salt VARCHAR(256) NOT NULL,
+  email_address VARCHAR(256) NOT NULL,
   PRIMARY KEY (id)
 );
 
