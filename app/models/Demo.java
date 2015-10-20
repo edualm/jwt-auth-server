dbd60db0ccbecd445026657a841ce698ab128c92
Eduardo Almeida <me@edr.io>
2015-10-20 15:43:57 +0100

    Forgot the @Entity...

--- app/models/Demo.java
+++ app/models/Demo.java
@@ -3,12 +3,15 @@ package models;
 import com.avaje.ebean.Model;
 import play.data.validation.Constraints;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 /**
  * Created by MegaEduX on 20/10/15.
  */
+
+@Entity
 public class Demo extends Model {
     @Id
     @GeneratedValue
