e484600d4ec1468f12d0a25438b1ca29407f6433
Eduardo Almeida <me@edr.io>
2015-10-20 17:46:15 +0100

    Added login debug.

--- app/controllers/Authenticate.java
+++ app/controllers/Authenticate.java
@@ -2,18 +2,33 @@ package controllers;
 
 import com.avaje.ebean.Model;
 import models.UserData;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.libs.Json;
 import play.mvc.*;
+
+import java.util.List;
 
 /**
  * Created by MegaEduX on 19/10/15.
  */
+
 public class Authenticate extends Controller {
 
     public Result login() {
+        DynamicForm form = Form.form().bindFromRequest();
+
+        String user = form.get("username");
+
         Model.Finder<Long, UserData> find = new Model.Finder<Long, UserData>(UserData.class);
 
-        return ok(Json.stringify(Json.toJson(find.findList())));
+        List<UserData> users = find.where().eq("username", user).findList();
+
+        if (users.size() == 0) {
+            return notFound("User not found!");
+        }
+
+        return ok(Json.stringify(Json.toJson(users.get(0))));
     }
 
 }
