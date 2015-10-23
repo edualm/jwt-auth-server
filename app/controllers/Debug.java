0f2143d23599e86307ec4d33dc08022656cfa9a4
Eduardo Almeida <me@edr.io>
2015-10-23 13:26:05 +0100

    Added test data call.

--- app/controllers/Debug.java
+++ app/controllers/Debug.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.avaje.ebean.Ebean;
+import models.Category;
 import models.UserData;
 import org.jose4j.lang.JoseException;
 import play.libs.Json;
@@ -26,5 +27,25 @@ public class Debug extends Controller {
         } catch (JoseException e) {
             return internalServerError(e.getMessage());
         }
+    }
+
+    public Result populateWithTestData() {
+        try {
+            UserData u = new UserData("foo", "bar", "foo@bar.com");
+            Category cat = new Category("Admin Category", Category.Type.Administrator);
+
+            u.addAttribute("fus, ro...", "DAH!");
+            u.categories.add(cat);
+
+            cat.addUser(u);
+
+            u.save();
+            cat.save();
+
+            return ok("{\"success\": \"true\"}");
+        } catch (Exception e) {
+            return ok("{\"success\": \"false\", \"debug\": \"" + e + "\"}");
+        }
+
     }
 }
