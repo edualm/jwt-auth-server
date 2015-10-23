3972090ecfa4949fcde872437c5dd930b1be12fb
Eduardo Almeida <me@edr.io>
2015-10-23 21:13:38 +0100

    Merged all evolutions, fixed up db.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,4 +1,4 @@
-# User Data table
+# DB Structure
 
 # --- !Ups
 
@@ -8,9 +8,45 @@ CREATE TABLE user_data (
   password_digest  VARCHAR(256) NOT NULL,
   password_salt VARCHAR(256) NOT NULL,
   email_address VARCHAR(256) UNIQUE NOT NULL,
+  enabled BOOLEAN NOT NULL DEFAULT TRUE,
+  signup_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE user_attribute (
+  id  SERIAL,
+  user_id INT REFERENCES user_data(id) NOT NULL,
+  key  VARCHAR(256) NOT NULL,
+  value TEXT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE configuration (
+  id  SERIAL,
+  key  VARCHAR(256) NOT NULL,
+  value TEXT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE category (
+  id  SERIAL,
+  name  VARCHAR(64) UNIQUE NOT NULL,
+  type  VARCHAR(32) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE category_user (
+  id  SERIAL,
+  category_id INT REFERENCES category(id) NOT NULL,
+  user_id INT REFERENCES user_data(id) NOT NULL,
+  CONSTRAINT category_user_pkey PRIMARY KEY (category_id, user_id),
   PRIMARY KEY (id)
 );
 
 # --- !Downs
 
 DROP TABLE user_data;
+DROP TABLE user_attribute;
+DROP TABLE configuration;
+DROP TABLE category;
+DROP TABLE category_user;
--- conf/evolutions/default/6.sql
+++
@@ -1,9 +0,0 @@
-# Drop username from User Attributes
-
-# --- !Ups
-
-ALTER TABLE user_attribute DROP COLUMN username
-
-# --- !Downs
-
-ALTER TABLE user_attribute ADD COLUMN username VARCHAR(64) UNIQUE NOT NULL;
