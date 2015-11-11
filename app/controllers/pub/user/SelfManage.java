044e289a3d654f0557ced33d8d1d33bc579f857b
Eduardo Almeida <me@edr.io>
2015-11-11 15:56:02 +0000

    Added Settings / Change Password UI.

--- app/controllers/pub/user/SelfManage.java
+++
@@ -1,7 +0,0 @@
-package controllers.pub.user;
-
-/**
- * Created by MegaEduX on 27/10/15.
- */
-public class SelfManage {
-}
---
+++ app/controllers/pub/user/Settings.java
@@ -0,0 +1,19 @@
+package controllers.pub.user;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by MegaEduX on 27/10/15.
+ */
+
+public class Settings extends Controller {
+
+    public Result handleSettings() {
+        return notFound();
+    }
+
+    public Result handlePasswordChange() {
+        return notFound();
+    }
+}
