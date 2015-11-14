81e5dc28bb4369424cb9953479407e2b3e2be3e2
Eduardo Almeida <me@edr.io>
2015-11-14 19:50:23 +0000

    Adding files...

--- app/controllers/pub/unauthenticated/RecoverPassword.java
+++ app/controllers/pub/unauthenticated/RecoverPassword.java
@@ -1,7 +1,11 @@
 package controllers.pub.unauthenticated;
 
+import play.mvc.Controller;
+
 /**
  * Created by MegaEduX on 13/11/15.
  */
-public class RecoverPassword {
+
+public class RecoverPassword extends Controller {
+
 }
