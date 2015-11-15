e3f17b7dce23eedb65ccc8f6d6e826ce2b285419
Eduardo Almeida <megaedux@me.com>
2015-11-15 20:18:29 +0000

    Done?

--- app/controllers/pub/unauthenticated/RecoverPassword.java
+++ app/controllers/pub/unauthenticated/RecoverPassword.java
@@ -1,11 +1,58 @@
 package controllers.pub.unauthenticated;
 
+import models.UserData;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.Controller;
+import play.mvc.Result;
+import utilities.Config;
+import utilities.KeyGenerator;
+import utilities.Mailer;
+import views.html.generic_failure;
+import views.html.generic_success;
+import views.html.password_recovery;
 
 /**
  * Created by MegaEduX on 13/11/15.
  */
 
 public class RecoverPassword extends Controller {
+
+    public Result forgotPage() {
+        return ok(password_recovery.render(Config.ServerName));
+    }
+
+    public Result handleForgot() {
+        DynamicForm form = Form.form().bindFromRequest();
+
+        String user = form.get("username").toLowerCase();
+        String email = form.get("email");
+
+        UserData u = UserData.getUserDataFromUsername(user);
+
+        if (u == null)
+            return notFound(generic_failure.render(Config.ServerName, "User not found!"));
+
+        if (!u.emailAddress.equals(email))
+            return notFound(generic_failure.render(Config.ServerName, "The inserted e-mail address is incorrect."));
+
+        KeyGenerator g = new KeyGenerator();
+
+        String newPassword = g.nextKey();
+
+        try {
+            u.changePassword(newPassword);
+        } catch (Exception e) {
+            return internalServerError(generic_failure.render(Config.ServerName, e.getMessage()));
+        }
+
+        Mailer m = new Mailer(Config.ServerName);
+
+        if (m.sendEmailPasswordChanged(u.username, u.emailAddress, newPassword)) {
+            return ok(generic_success.render(Config.ServerName, "A new password has been sent to your e-mail address."));
+        } else {
+            return internalServerError(generic_success.render(Config.ServerName, "Unable to send an e-mail. Please try again."));
+        }
+    }
 
 }
